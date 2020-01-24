869717fbb0e1104a6d41b546e20b24877a597466
janfernandosmoothstack <janet.fernando@smoothstack.com>
2020-01-24 13:21:13 -0500

    first commit

---
+++ src/main/SortingChallenge.java
@@ -0,0 +1,12 @@
+package main;
+
+public class SortingChallenge {
+
+	public static void main(String[] args) {
+		String[] names = {"Adams", "Quincy", "Washington", "Allison", "Jones", "Ward", 
+				"Johnson", "Barnes", "Calvin", "Zigler", "Ball"};
+		
+		String[] sortedNames = new String[11];
+	}
+
+}
